ece7bc71036934cc97d5851b11b943adbe097d06
Alan Eicker <alaneicker@hotmail.com>
2018-09-20 10:28:27 -0500

    Update queries.js

--- server/queries.js
+++ server/queries.js
@@ -1,30 +1,5 @@
-// Mongoose
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
-
-const contactSchema = new Schema({
-    id: { type: Number, required: true, default: '' },
-    name: {
-        first: { type: String, required: true, default: '' },
-        last: { type: String, required: true, default: '' },
-        mi: { type: String, required: true, default: '' }
-    },
-    address: {
-        addr1: { type: String, required: true, default: '' },
-        addr2: { type: String, required: true, default: '' },
-        addr2_type: { type: String, required: true, default: '' },
-        city: { type: String, required: true, default: '' },
-        state: { type: String, required: true, default: '' },
-        zip: { type: String, required: true, default: '' }
-    },
-    job_title: { type: String, required: true, default: '' },
-    phone: { type: String, required: true, default: '' },
-    email: { type: String, required: true, default: '' },
-    bio: { type: String, required: true, default: '' },
-    img_url: { type: String, required: true, default: '' }
-});
-
+const contactSchema = require('contact-list-schema');
 const contacts = mongoose.model('contact_list', contactSchema);
 
 mongoose.connect('mongodb://localhost/contacts');
