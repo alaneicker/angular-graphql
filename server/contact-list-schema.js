4b301196bce558ec8c01341edb88c9f951a50202
Alan Eicker <alaneicker@hotmail.com>
2018-09-20 10:26:20 -0500

    Create contact-list-schema.js

---
+++ server/contact-list-schema.js
@@ -0,0 +1,24 @@
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+module.exports = new Schema({
+    id: { type: Number, required: true, default: '' },
+    name: {
+        first: { type: String, required: true, default: '' },
+        last: { type: String, required: true, default: '' },
+        mi: { type: String, required: true, default: '' }
+    },
+    address: {
+        addr1: { type: String, required: true, default: '' },
+        addr2: { type: String, required: true, default: '' },
+        addr2_type: { type: String, required: true, default: '' },
+        city: { type: String, required: true, default: '' },
+        state: { type: String, required: true, default: '' },
+        zip: { type: String, required: true, default: '' }
+    },
+    job_title: { type: String, required: true, default: '' },
+    phone: { type: String, required: true, default: '' },
+    email: { type: String, required: true, default: '' },
+    bio: { type: String, required: true, default: '' },
+    img_url: { type: String, required: true, default: '' }
+});
