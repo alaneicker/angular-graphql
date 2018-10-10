7ac31b60a266739e4363a31c5cbf74fef2c3d4e2
Alan Eicker <alaneicker@gmail.com>
2018-10-10 08:40:12 -0500

    makes fullname pipe impure

--- src/app/pipes/fullname.pipe.ts
+++ src/app/pipes/fullname.pipe.ts
@@ -1,6 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({name: 'fullname'})
+@Pipe({
+  name: 'fullname',
+  pure: false,
+})
 export class FullNamePipe implements PipeTransform {
   transform(name: any): string {
     return name ? `${name.first} ${name.mi || ''} ${name.last}` : null;
